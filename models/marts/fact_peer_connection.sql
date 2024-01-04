6c7b62a074c24ca82cf502b84b04a2e7b9c9bec4
SunriseLong <sunrise.long@yahoo.ca>
2024-01-04 18:55:36 -0500

    add node tracker dimentions

--- models/marts/fact_peer_connection.sql
+++ models/marts/fact_peer_connection.sql
@@ -75,7 +75,7 @@ WITH peer_set_unioned AS (
           sessions.end_timestamp,
           (SELECT MAX(msg_timestamp) FROM SESSIONS)
         )                                               AS end_time
-      , session_duration                                
+      , session_duration::NUMBER                        AS session_duration                            
       , DATEDIFF(NANOSECOND, start_time, end_time)      AS session_duration_calculated
       , sessions.end_timestamp IS NULL                  AS end_time_imputed
       , node_id
@@ -83,3 +83,37 @@ WITH peer_set_unioned AS (
   FROM sessions
   WHERE msg_type = 'peer_set_add'
 )
+, node_tracker AS (
+  SELECT
+      node_id
+    , msg_timestamp
+    , peer_id
+    , msg_data        AS peer_meta_data
+  FROM {{ source('keystone_offchain', 'network_feed') }}
+  -- TODO: Remove for prod
+    WHERE msg_timestamp >= '2023-12-26'
+        AND msg_type = 'node_tracker'
+)
+SELECT 
+    s.start_time
+  , s.end_time
+  , s.session_duration
+  , s.session_duration_calculated
+  , s.end_time_imputed
+  , s.node_id
+  , s.peer_id
+  , REPLACE(peer_meta_data[0], '"', '')                 AS peer_public_key
+  , peer_meta_data[1]::INT                              AS peer_rlp_protocol_version
+  , NULLIF(SPLIT_PART(peer_meta_data[2], '/', 0), '')   AS client_type
+  , NULLIF(SPLIT_PART(peer_meta_data[2], '/', 1), '')   AS client_version
+  , NULLIF(SPLIT_PART(peer_meta_data[2], '/', 2), '')   AS os
+  , NULLIF(SPLIT_PART(peer_meta_data[2], '/', 3), '')   AS run_time_version
+  , REPLACE(peer_meta_data[3], '"', '')                 AS peer_capabilities
+  -- bug with split_part
+  , REPLACE(SPLIT(peer_meta_data[4], ':')[0], '"', '')  AS peer_ip
+  , SPLIT(peer_meta_data[4], ':')[1]::INT               AS peer_port
+FROM sessions_enriched s
+  LEFT JOIN node_tracker nt
+    ON s.node_id = nt.node_id
+      AND s.peer_id = nt.peer_id
+      AND DATEDIFF(MINUTES, s.start_time, nt.msg_timestamp) BETWEEN 0 AND 1

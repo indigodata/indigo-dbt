6f672e33a0a1938964496c055a139388741adf94
SunriseLong <sunrise.long@yahoo.ca>
2024-01-04 10:32:35 -0500

    mart model

---
+++ models/marts/fact_transaction__tx_hash.sql
@@ -0,0 +1,24 @@
+{{
+    config(
+        materialized='incremental'
+      , cluster_by=['tx_hash']
+      , tags=['incremental_coordinator']
+      , post_hook='{{ update_run_completed() }}'
+    )
+}}
+
+SELECT
+    blk_number
+  , blk_timestamp
+  , blockchain
+  , tx_hash
+  , tx_index
+  , tx_status
+  , tx_from
+  , tx_to
+  , tx_gas_price
+  , tx_gas_used
+  , tx_value
+  , run_timestamp
+FROM {{ ref('stg_keystone__eth_transaction') }}
+WHERE {{ new_blocks_only() }}

f5b5f1aa4860f1becf3305bc898bcd2f7bf60a31
SunriseLong <sunrise.long@yahoo.ca>
2024-03-14 14:31:23 -0400

    export undersampled bootnodes

---
+++ models/marts/boot_node.sql
@@ -0,0 +1,36 @@
+{{
+    config(
+        materialized='incremental'
+      , cluster_by=['peer_id']
+      , post_hook=['{{ export_boot_node_to_s3() }}']
+    )
+}}
+
+WITH latest_session AS (
+    SELECT
+        peer_id,
+        peer_region,
+        ROW_NUMBER() OVER(PARTITION BY peer_id ORDER BY start_time DESC) AS row_num
+    FROM {{ ref('fact_peer_session') }}
+    QUALIFY row_num = 1
+)
+, hash_ct AS (
+    SELECT
+          latest_session.peer_region
+        , hourly.peer_id
+        , SUM(hourly.total_hash_count) AS hash_ct
+    FROM {{ ref('peer_session_performance_hourly') }} hourly
+        LEFT JOIN latest_session
+            ON hourly.peer_id = latest_session.peer_id
+    WHERE latest_session.peer_region IS NOT NULL
+    GROUP BY 1,2
+    HAVING hash_ct > 0
+)
+SELECT
+      peer_region
+    , peer_id
+    , hash_ct
+    , '{{run_started_at}}'::timestamp_ntz AS updated_at
+FROM hash_ct
+QUALIFY ROW_NUMBER() OVER(PARTITION BY peer_region ORDER BY hash_ct) <= 100
+ORDER BY peer_region, hash_ct DESC

3add30fc92294e7b65cd596dc2dac1c99af8a263
SunriseLong <44146580+SunriseLong@users.noreply.github.com>
2024-04-09 15:52:20 -0400

    Boot Node (#16)
    * add msg per min
    
    * fix hash_count naming
    
    * wip peer_session_unique_hash
    
    * model peer_session_performance_hourly
    
    * add new msg type: new_hash
    
    * udpate date range
    
    * Refactor fact_peer_session to incremental w/ incomplete sessions
    
    * lint fact_peer_session
    
    * fix node_tracker_feed exploding join
    
    * peer_session_performance_houly => incremental
    
    * peer_session_performance => incremental
    
    * add peer_region to seed and fps
    
    * export undersampled bootnodes
    
    * peer_id => enode
    
    * set boot node lookback window to 12 days
    
    * rename boot_node => undersampled_peers
    
    * update references
    
    * productions => production
    
    * fix bad .sql rename
    
    * rename daily selector => undersampled peers
    
    * ORDER BY desc => asc
    
    * export fix production schema check

---
+++ macros/export_utils/export_to_s3.sql
@@ -0,0 +1,42 @@
+{% macro export_to_s3(stage, file_name, query) -%}
+    {% set stage_name = '@warehouse_db.production.' ~ stage %}
+    {% set copy_query %}
+        COPY INTO {{ stage_name }}/{{ file_name }}
+        FROM (
+            {{ query }}
+        )
+        FILE_FORMAT = (
+            TYPE = 'CSV'
+            EMPTY_FIELD_AS_NULL = FALSE
+            NULL_IF = ('')
+        )
+        SINGLE=TRUE
+        OVERWRITE=TRUE
+        HEADER=TRUE
+        MAX_FILE_SIZE = 52428800;
+    {% endset %}
+
+    {% if execute %}
+        {% if should_full_refresh() and 'full_refresh' not in stage.lower() %}
+            {{ log('This is a full refresh. Please use the full-refresh stage and copy it manually. Skipping the export.', 'warn') }}
+        {% elif target.schema.lower() != 'production' %}
+            {{ log('Not in the production schema. Skipping the export.', 'info') }}
+        {% else %}
+            {{ log('Copying data into S3 bucket', 'info') }}
+            {% do log(copy_query, info=True) %}
+            {% do run_query(copy_query) %}
+        {% endif %}
+    {% endif %}
+{%- endmacro %}
+
+{% macro export_undersampled_peers_to_s3() -%}
+    {% set query %}
+        SELECT 
+              peer_region
+            , enode
+            , hash_ct
+        FROM production.undersampled_peers
+        WHERE updated_at = '{{ run_started_at }}'::timestamp_ntz
+    {% endset %}
+    {{ export_to_s3('s3_export_stage', 'undersampled_peers/undersampled_peers.csv.gz', query)}}
+{%- endmacro %}
---
+++ models/marts/undersampled_peers.sql
@@ -0,0 +1,36 @@
+{{
+    config(
+        materialized='incremental'
+      , post_hook=['{{ export_undersampled_peers_to_s3() }}']
+    )
+}}
+
+WITH latest_session AS (
+    SELECT
+          peer_id
+        , peer_region
+        , peer_public_key || '@' || peer_ip || ':' || peer_port AS enode
+        , ROW_NUMBER() OVER(PARTITION BY peer_id ORDER BY start_time DESC) AS row_num
+    FROM {{ ref('fact_peer_session') }}
+    QUALIFY row_num = 1
+)
+, hash_ct AS (
+    SELECT
+          COALESCE(latest_session.peer_region, 'unknown')   AS peer_region
+        , latest_session.enode
+        , SUM(hourly.total_hash_count)                      AS hash_ct
+    FROM {{ ref('peer_session_performance_hourly') }} hourly
+        LEFT JOIN latest_session
+            ON hourly.peer_id = latest_session.peer_id
+    WHERE hourly.session_hour >= dateadd(DAY, -12, SYSDATE())
+    GROUP BY 1,2
+    HAVING hash_ct > 0
+)
+SELECT
+      peer_region
+    , enode
+    , hash_ct
+    , '{{run_started_at}}'::timestamp_ntz AS updated_at
+FROM hash_ct
+QUALIFY ROW_NUMBER() OVER(PARTITION BY peer_region ORDER BY hash_ct) <= 100
+ORDER BY peer_region, hash_ct

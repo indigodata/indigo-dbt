0e881a8819e3506a90008f4b0355fdbd096591d5
SunriseLong <sunrise.long@yahoo.ca>
2024-04-09 10:18:25 -0400

    ORDER BY desc => asc

--- models/marts/undersampled_peers.sql
+++ models/marts/undersampled_peers.sql
@@ -16,14 +16,13 @@ WITH latest_session AS (
 )
 , hash_ct AS (
     SELECT
-          latest_session.peer_region
+          COALESCE(latest_session.peer_region, 'unknown')   AS peer_region
         , latest_session.enode
-        , SUM(hourly.total_hash_count) AS hash_ct
+        , SUM(hourly.total_hash_count)                      AS hash_ct
     FROM {{ ref('peer_session_performance_hourly') }} hourly
         LEFT JOIN latest_session
             ON hourly.peer_id = latest_session.peer_id
     WHERE hourly.session_hour >= dateadd(DAY, -12, SYSDATE())
-        AND latest_session.peer_region IS NOT NULL
     GROUP BY 1,2
     HAVING hash_ct > 0
 )
@@ -34,4 +33,4 @@ SELECT
     , '{{run_started_at}}'::timestamp_ntz AS updated_at
 FROM hash_ct
 QUALIFY ROW_NUMBER() OVER(PARTITION BY peer_region ORDER BY hash_ct) <= 100
-ORDER BY peer_region, hash_ct DESC
+ORDER BY peer_region, hash_ct

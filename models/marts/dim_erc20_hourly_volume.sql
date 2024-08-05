f36296e28544c2da72f813993d52902d04b2c2df
SunriseLong <sunrise.long@yahoo.ca>
2024-08-05 17:12:28 -0400

    filter swaps from hourly volume

--- models/marts/dim_erc20_hourly_volume.sql
+++ models/marts/dim_erc20_hourly_volume.sql
@@ -4,38 +4,38 @@
       , cluster_by=['token_contract']
     )
 }}
-WITH erc_transfer AS (
+WITH pair_swaps AS (
+    SELECT
+          tx_hash
+        , COUNT(DISTINCT token_contract) AS distinct_token_ct
+    FROM {{ ref('fact_erc20_transfer') }}
+    GROUP BY 1
+    HAVING distinct_token_ct >= 2
+)
+, erc_transfer AS (
     SELECT
           erc.tx_hash
         , erc.token_contract
-        , ANY_VALUE(erc.blk_timestamp)                  AS blk_timestamp
+        , ANY_VALUE(erc.blk_timestamp)                          AS blk_timestamp
         , SUM(
-            IFF(
-                erc.tx_from = erc.recipient,
-                erc.amount / POW(10, price.token_decimals) * price.start_price_eth,
-                0
-            )
-          )                                         AS in_eth
+            IFF(erc.tx_from = erc.recipient, erc.amount_eth, 0)
+          )                                                     AS in_eth
         , SUM(
-            IFF(
-                erc.tx_from = erc.sender,
-                erc.amount / POW(10, price.token_decimals) * price.start_price_eth,
-                0
-            )
-          )                                         AS out_eth
-        , in_eth - out_eth                          AS net_eth
+            IFF(erc.tx_from = erc.sender, erc.amount_eth, 0)
+          )                                                     AS out_eth
+        , in_eth - out_eth                                      AS net_eth
     FROM {{ ref('fact_erc20_transfer') }} erc
-        LEFT JOIN {{ ref('fact_hourly_price_change') }} price
-            ON erc.token_contract = price.token_address
-                AND DATE_TRUNC(HOUR, erc.blk_timestamp) = price.start_hour
+        INNER JOIN pair_swaps pair
+            ON erc.tx_hash = pair.tx_hash
         WHERE (erc.tx_from = erc.sender 
             OR erc.tx_from = erc.recipient)
-        AND blk_timestamp >= '2024-01-01 01:00:00'
+            AND blk_timestamp >= '2024-01-01 01:00:00'
     GROUP BY 1,2
 )
 SELECT 
       erc.token_contract
     , DATE_TRUNC(HOUR, erc.blk_timestamp)       AS transfer_hour
+    , peer.peer_country
     , IFF(peer.peer_country = 'United States',
         peer.peer_subdivision,
         peer.peer_country
@@ -48,4 +48,4 @@ FROM erc_transfer erc
         ON erc.tx_hash = '0x' || lower(win.tx_hash::VARCHAR)
     LEFT JOIN production.dim_peer_performance peer
         ON win.peer_id = peer.peer_id
-GROUP BY 1,2,3
+GROUP BY 1,2,3,4
